50947eca78aa203cf7781fcbaf76d5dcb9fcd09d
nataliodg <natadg@outlook.com>
2023-01-12 15:17:54 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeContextProvider } from './context/ThemeContext'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeContextProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeContextProvider>
 )
